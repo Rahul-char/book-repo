c3807ac324dc19fcc6ca3630dcc787140a088623
Rahul <rahul.kasoju@gmail.com>
2024-10-30 11:23:11 +0530

    testCommit

--- src/main/java/com/TuesdayProject/books/Controller/bookController.java
+++ src/main/java/com/TuesdayProject/books/Controller/bookController.java
@@ -36,4 +36,14 @@ public class bookController {
     public ResponseEntity<List<bookModel>> getbookModelbyAuthorName(@PathVariable("author") String author){
         return new ResponseEntity<>(book.getbooksbyauthor(author), HttpStatus.OK);
     }
+
+//    @GetMapping("/getAllUniversebooks/{bookUniverse}")
+//    public ResponseEntity<List<bookModel>> getAllUniverseBooks(@PathVariable("bookUniverse") String bookUniverse){
+//        return new ResponseEntity<>(book.getAllUniverseBooks(bookUniverse), HttpStatus.OK);
+//    }
+//
+//    @GetMapping("/getbooksByauthor/{author}")
+//    public ResponseEntity<List<bookModel>> getbookModelbyAuthorName(@PathVariable("author") String author){
+//        return new ResponseEntity<>(book.getbooksbyauthor(author), HttpStatus.OK);
+//    }
 }
